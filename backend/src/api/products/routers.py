c870623a612d3637395fc44f6bdf78196724d176
drik-exe <27danik98@gmail.com>
2024-09-28 14:05:44 +0300

    added service for products

--- backend/src/api/products/routers.py
+++ backend/src/api/products/routers.py
@@ -7,6 +7,7 @@ from sqlalchemy.future import select
 
 from src.api.products.models import Product
 from src.api.products.schemas import ProductCreate, ProductResponse, ProductUpdate
+from src.api.products.service import create, get, get_by_name, update
 from src.api.products.utils import save_image
 from src.db import get_session
 
@@ -17,36 +18,17 @@ router = APIRouter(prefix="/products", tags=["Products"])
 async def create_product(
     product: ProductCreate, file: UploadFile | None = File(None), db: AsyncSession = Depends(get_session)
 ) -> dict[str, Any]:
-    if file:
-        image_url = await save_image(file)
-    else:
-        image_url = None
-    async with db as session:
-        async with session.begin():
-            new_product = Product(
-                name=product.name, description=product.description, price=product.price, image_url=image_url
-            )
-            session.add(new_product)
-        return {"message": "Product created successfully"}
+    return await create(product, file, db)
 
 
 @router.get("/get_products", response_model=list[ProductResponse])
 async def get_products(db: AsyncSession = Depends(get_session)) -> Sequence[Product]:
-    async with db as session:
-        query = select(Product)
-        results = await session.execute(query)
-        return results.scalars().all()
+    return await get(db)
 
 
 @router.get("/get_product_by_name/{product_name}", response_model=ProductResponse)
 async def get_product_by_name(product_name: str, db: AsyncSession = Depends(get_session)) -> Product:
-    async with db as session:
-        query = select(Product).where(product_name == Product.name)
-        result = await session.execute(query)
-        product = result.scalar()
-        if product:
-            return product
-        raise HTTPException(status_code=404, detail="Product not found")
+    return await get_by_name(product_name, db)
 
 
 # Добавить фото
@@ -56,22 +38,4 @@ async def update_product(
         file: UploadFile | None = File(None),
         db: AsyncSession = Depends(get_session)
 ) -> dict[str, Any]:
-    if file:
-        image_url = await save_image(file)
-    else:
-        image_url = None
-    async with db as session:
-        product = await session.get(Product, product_id)
-        if product:
-            if product_data.name:
-                product.name = product_data.name
-            if product_data.description:
-                product.description = product_data.description
-            if product_data.price:
-                product.price = product_data.price
-            if image_url:
-                product.image_url = image_url
-
-            await session.commit()
-            return {"message": "Product updated successfully"}
-        raise HTTPException(status_code=404, detail="Product not found")
+    return await update(product_id, product_data, file, db)
