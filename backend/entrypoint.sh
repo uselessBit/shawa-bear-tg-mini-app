2c54abf41a1bcd2edcaa25d080cfc586eedaa1c7
d.drik <d.drik@tbank.ru>
2024-11-17 17:28:18 +0300

    fix path to run uvicorn

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 alembic upgrade head
 
-uvicorn src.api.main:app --host 0.0.0.0 --port 443 --ssl-keyfile localhost-key.pem --ssl-certfile localhost.pem
+uvicorn src.api.server.app:create_application --factory --host 0.0.0.0 --port 443 --ssl-keyfile localhost-key.pem --ssl-certfile localhost.pem
