dec5c89ccaf30a02a494a207c4c8037a1a978dcb
usagi. <solevoi.ghoul@gmail.com>
2024-11-22 11:05:41 +0300

    Update entrypoint.sh

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -2,3 +2,4 @@
 alembic upgrade head
 
 uvicorn src.api.server.app:create_application --factory --host 0.0.0.0 --port 443 --ssl-keyfile localhost-key.pem --ssl-certfile localhost.pem
+# uvicorn src.server.app:create_application --factory --host 127.0.0.1 --port 443 --ssl-keyfile localhost-key.pem --ssl-certfile localhost.pem
