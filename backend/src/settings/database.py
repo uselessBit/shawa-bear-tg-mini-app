d8efbca5a22bad985b395b3ab8557a4c687f8a1e
d.drik <d.drik@tbank.ru>
2024-11-25 14:33:52 +0300

    fix docker-compose and database settings

--- backend/src/settings/database.py
+++ backend/src/settings/database.py
@@ -1,18 +1,4 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
-
-
-class DatabaseSettings(BaseSettings):
-    host: str = "localhost"
-    port: int = 5432
-    name: str = "mini_app"
-    user: str = "postgres"
-    password: str = "admin"
-    driver: str = "asyncpg"
-    type: str = "postgresql"
-    url: str = f"{type}+{driver}://{user}:{password}@{host}:{port}/{name}"
-
-    model_config = SettingsConfigDict(env_prefix="db_")
-from pydantic_settings import BaseSettings, SettingsConfigDict
 from dotenv import load_dotenv
 import os
 
