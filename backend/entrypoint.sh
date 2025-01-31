d5b070bcc76d1343ad179e52b5ea646f5bd875e0
d.drik <d.drik@tbank.ru>
2025-01-31 14:14:09 +0300

    some changes

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -2,4 +2,3 @@
 alembic upgrade head
 
 uvicorn src.server.app:create_application --factory --host 0.0.0.0 --port 8000
-# uvicorn src.server.app:create_application --factory --host 127.0.0.1 --port 443 --ssl-keyfile localhost-key.pem --ssl-certfile localhost.pem
