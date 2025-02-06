495a169a2cdb7c9b44f7627a512b7be0e4ce3fe4
d.drik <d.drik@tbank.ru>
2025-02-06 14:10:22 +0300

    add only postgres in docker support

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 alembic upgrade head
 
-uvicorn src.server.app:create_application --factory --host 0.0.0.0 --port 8000
-
+#uvicorn src.server.app:create_application --factory --host 0.0.0.0 --port 8000
+python3 -m src
