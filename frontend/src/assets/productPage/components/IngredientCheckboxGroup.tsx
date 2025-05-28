c257af611ead893900834077b021333500afc452
usagi <solevoi.ghoul@gmail.com>
2025-05-28 20:50:03 +0300

    fix ingredients

--- frontend/src/assets/productPage/components/IngredientCheckboxGroup.tsx
+++ frontend/src/assets/productPage/components/IngredientCheckboxGroup.tsx
@@ -37,59 +37,64 @@ export default function IngredientCheckboxGroup({
                 Состав
             </Heading>
             <Flex wrap="wrap" gap="8px">
-                {ingredients.slice(1).map((ingredient) => (
-                    <CheckboxCard.Root
-                        key={ingredient.ingredient_id}
-                        cursor="pointer"
-                        minW="fit-content"
-                        maxW="fit-content"
-                        border="none"
-                        rounded="full"
-                        bg={`${ingredient.color}/10`}
-                        position="relative"
-                        _checked={{
-                            boxShadow: 'none',
-                            '& .checkbox-control': {
-                                opacity: 0.5,
-                            },
-                            '&::after': {
-                                opacity: '1',
-                                content: '""',
-                                position: 'absolute',
-                                left: '16px',
-                                bottom: '16px',
-                                width: 'calc(100% - 32px)',
-                                height: '2px',
-                                bg: 'text',
-                                transform: 'rotate(-2deg)',
-                                transformOrigin: 'left bottom',
-                            },
-                        }}
-                    >
-                        <CheckboxCard.HiddenInput
-                            onChange={(e) =>
-                                handleCheckboxChange(
-                                    ingredient.ingredient_id,
-                                    e.target.checked
-                                )
-                            }
-                        />
-                        <CheckboxCard.Control
-                            px="16px"
-                            py="8px"
-                            className="checkbox-control"
-                        >
-                            <CheckboxCard.Label>
-                                <Image
-                                    src={`${API_BASE_URL}media/ingredients/${ingredient.image_url}`}
-                                    h="24px"
-                                />
-                                <Text>{ingredient.name}</Text>
-                                <IoClose />
-                            </CheckboxCard.Label>
-                        </CheckboxCard.Control>
-                    </CheckboxCard.Root>
-                ))}
+                {ingredients.map(
+                    (ingredient) =>
+                        ingredient.type !== 'base' && (
+                            <>
+                                <CheckboxCard.Root
+                                    key={ingredient.ingredient_id}
+                                    cursor="pointer"
+                                    minW="fit-content"
+                                    maxW="fit-content"
+                                    border="none"
+                                    rounded="full"
+                                    bg={`${ingredient.color}/10`}
+                                    position="relative"
+                                    _checked={{
+                                        boxShadow: 'none',
+                                        '& .checkbox-control': {
+                                            opacity: 0.5,
+                                        },
+                                        '&::after': {
+                                            opacity: '1',
+                                            content: '""',
+                                            position: 'absolute',
+                                            left: '16px',
+                                            bottom: '16px',
+                                            width: 'calc(100% - 32px)',
+                                            height: '2px',
+                                            bg: 'text',
+                                            transform: 'rotate(-2deg)',
+                                            transformOrigin: 'left bottom',
+                                        },
+                                    }}
+                                >
+                                    <CheckboxCard.HiddenInput
+                                        onChange={(e) =>
+                                            handleCheckboxChange(
+                                                ingredient.ingredient_id,
+                                                e.target.checked
+                                            )
+                                        }
+                                    />
+                                    <CheckboxCard.Control
+                                        px="16px"
+                                        py="8px"
+                                        className="checkbox-control"
+                                    >
+                                        <CheckboxCard.Label>
+                                            <Image
+                                                src={`${API_BASE_URL}media/ingredients/${ingredient.image_url}`}
+                                                h="24px"
+                                            />
+                                            <Text>{ingredient.name}</Text>
+                                            <IoClose />
+                                        </CheckboxCard.Label>
+                                    </CheckboxCard.Control>
+                                </CheckboxCard.Root>
+                            </>
+                        )
+                )}
             </Flex>
         </CheckboxGroup>
     )
