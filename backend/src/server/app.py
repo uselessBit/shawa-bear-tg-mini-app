2c60f14af24bb2668490edb1ec56997e2087b1c5
d.drik <27danik98@gmail.com>
2025-05-30 00:26:35 +0300

    add admin in app

--- backend/src/server/app.py
+++ backend/src/server/app.py
@@ -1,8 +1,18 @@
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
-from starlette.middleware.cors import CORSMiddleware
 
-from src.container import DependencyContainer
+from src.admin.models.basket_admin import BasketAdmin, BasketItemAdmin, BasketItemExcludedIngredientAdmin
+from src.admin.models.category_admin import CategoryAdmin
+from src.admin.models.ingredient_admin import IngredientAdmin
+from src.admin.models.order_admin import OrderAdmin
+from src.admin.models.price_admin import PriceAdmin
+from src.admin.models.product_admin import ProductAdmin, ProductIngredientAdmin
+from src.admin.models.size_admin import SizeAdmin
+from src.admin.models.user_admin import UserAdmin
+from starlette.middleware.cors import CORSMiddleware
+from sqladmin import Admin
+
+from src.container import DependencyContainer, container
 from src.server.handle_erros import patch_exception_handlers
 from src.server.routers.v1.routers import api_v1_router
 
@@ -27,6 +37,20 @@ def create_application() -> CustomFastAPI:
         allow_methods=["*"],
         allow_headers=["*"],
     )
+
+    admin = Admin(server, engine=container.database().engine)
+    admin.add_view(UserAdmin)
+    admin.add_view(IngredientAdmin)
+    admin.add_view(OrderAdmin)
+    admin.add_view(PriceAdmin)
+    admin.add_view(ProductAdmin)
+    admin.add_view(ProductIngredientAdmin)
+    admin.add_view(SizeAdmin)
+    admin.add_view(CategoryAdmin)
+    admin.add_view(BasketAdmin)
+    admin.add_view(BasketItemAdmin)
+    admin.add_view(BasketItemExcludedIngredientAdmin)
+
     patch_exception_handlers(app=server)
     server.mount("/media", StaticFiles(directory="/media"), name="media")
     server.include_router(api_v1_router)
